d2b0104d1c04984c133c7342896ccf97b143a5be
Jakob Haastert <Haastert.Jakob@scheidt-bachmann-st.de>
2025-01-23 13:19:42 +0100

    fixed a few issues, made the mojo work, reverted the indentation for untouched files

---
+++ osgi/de.cau.cs.kieler.spviz.osgi.generate/src/de/cau/cs/kieler/spviz/osgi/generate/util/EdgeType.java
@@ -0,0 +1,18 @@
+// ******************************************************************************
+//
+// Copyright (c) 2018-2025 by
+// Scheidt & Bachmann System Technik GmbH, 24145 Kiel
+//
+// This program and the accompanying materials are made available under the
+// terms of the Eclipse Public License 2.0 which is available at
+// http://www.eclipse.org/legal/epl-2.0.
+// 
+// SPDX-License-Identifier: EPL-2.0
+//
+// ******************************************************************************
+
+package de.cau.cs.kieler.spviz.osgi.generate.util;
+
+public enum EdgeType {
+	BUNDLE_DEPENDENCY, PACKAGE_DEPENDENCY
+}
